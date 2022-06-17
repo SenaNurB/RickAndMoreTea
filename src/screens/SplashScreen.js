432b750b2a6ab0eaf5a485a9d783317c24b65fe1
Sena Nur BAYIR <senanurbayir@192.168.1.101>
2022-06-17 04:13:31 +0300

    Splash Screen eklendi

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,34 @@
+import { StyleSheet, View, Image } from "react-native";
+import React, { useEffect } from "react";
+
+const SplashScreen = ({ navigation }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate("Home");
+    }, 1000);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require("../images/rickAndMortyyy.png")}
+        style={styles.image}
+      />
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#1B2430",
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+});
