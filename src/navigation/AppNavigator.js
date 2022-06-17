6411bc21068436c971bee0dc3789ba79a053813a
Sena Nur BAYIR <senanurbayir@192.168.1.101>
2022-06-17 04:13:31 +0300

    Splash Screen eklendi

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "@screens/HomeScreen";
 import EpisodeScreen from "@screens/EpisodeScreen";
 import CharacterDetail from "@screens/CharacterDetail";
+import SplashScreen from "@screens/SplashScreen";
 
 import { screenOptions } from "./config";
 
@@ -15,6 +16,11 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={screenOptions}>
+        <Stack.Screen
+          name="SplashScreen"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Episode" component={EpisodeScreen} />
         <Stack.Screen name="Character Detail" component={CharacterDetail} />
